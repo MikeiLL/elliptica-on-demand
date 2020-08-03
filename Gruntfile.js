a8544147a3c52d5daaa14c5bc62c0be2f5ef1dee
Mike iLL Kilmer <mike@mzoo.org>
2020-08-03 17:34:31 -0500

    Set up grunt and add isotope.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,88 @@
 'use strict';
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
+
+  // Project configuration.
   grunt.initConfig({
-    compass: {
-      css: {
-        options: {
-          sassDir: 'assets/sass',
-          cssDir: 'assets/css',
-          environment: 'production',
-          relativeAssets: true
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+    	options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		  },
+		main: {
+			src: 'assets/js/<%= pkg.name %>.js',
+			dest: 'dist/js/<%= pkg.name %>.min.js'
+		},
+		isotope: {
+			src: 'node_modules/isotope-layout/dist/isotope.pkgd.js',
+        	dest: 'dist/js/isotope.min.js'
         }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
       },
-      cssDev: {
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!dist/*'
+      ]
+    },
+    sass: {
+      main: {
         options: {
-          environment: 'development',
-          debugInfo: true,
-          noLineComments: false,
-          sassDir: 'assets/sass',
-          cssDir: 'assets/css',
-          outputStyle: 'expanded',
-          relativeAssets: true,
-          sourcemap: true
-        }
+          style: 'compressed',
+          compass: false,
+          sourcemap: false
+        },
+        files: [
+			{src: ['assets/sass/<%= pkg.name %>.scss'], dest: 'dist/css/<%= pkg.name %>.min.css'},
+			{src: ['assets/sass/settings.scss'], dest: 'dist/css/settings.css'},
+		  ]
       }
     },
     watch: {
-      compass: {
+      options: {
+        livereload: true
+      },
+      sass: {
         files: [
-          'assets/sass/*.scss',
-          'assets/coffee/*.coffee'
+          'assets/sass/*.scss'
         ],
-        tasks: [
-          'compass:css', 'compass:cssDev',
-          'coffee:js', 'coffee:jsDev'
-        ]
+        tasks: ['sass']
+      },
+      js: {
+        files: [
+          'assets/js/*.js'
+        ],
+        tasks: ['jshint', 'uglify']
       }
+    },
+    clean: {
+      dist: [
+        'dist/css/<%= pkg.name %>.min.css',
+        'dist/js/<%= pkg.name %>.min.js'
+      ]
     }
   });
 
   // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Register tasks
   grunt.registerTask('default', [
-    'compass:css', 'compass:cssDev'
+    'clean',
+    'sass',
+    'uglify'
   ]);
   grunt.registerTask('dev', [
     'watch'
   ]);
 
 };
+
+
