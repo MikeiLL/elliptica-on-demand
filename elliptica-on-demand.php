a64c088e4944124baad266453fad2777de421d81
Mike iLL Kilmer <mike@mzoo.org>
2021-05-20 10:03:17 -0500

    Remove gremins from working with REST example.

--- elliptica-on-demand.php
+++ elliptica-on-demand.php
@@ -38,7 +38,7 @@ define( 'MMC_PLUGIN_ABSOLUTE', __FILE__ );
 define( 'MMC_MIN_PHP_VERSION', 7.2 );
 define( 'MMC_MIN_WP_VERSION',  5.3 );
 
-example = new Rest\Example;
+//$example = new Rest\Example;
 
 add_action(
     'init',
