7a0cfbcf1cc1182a01a8d465cfec66df2895d135
Mike iLL Kilmer <mike@mzoo.org>
2021-05-25 10:56:55 -0500

    Small updates.

--- functions/debug.php
+++ functions/debug.php
@@ -21,7 +21,7 @@ $mmc_debug = new WPBP_Debug( __( 'Elliptica On Demand', MMC_TEXTDOMAIN ) );
 function mmc_log( $text ) {
 	global $mmc_debug;
 	$mmc_debug->log( $text );
-	// $mmc_debug->qm_log( __( 'Error inside the log panel of Query Monitor', 'your-textdomain' ), 'error' );
+	$mmc_debug->qm_log( __( 'Error inside the log panel of Query Monitor', 'your-textdomain' ), 'error' );
 	// $mmc_debug->qm_timer( 'profile_that_callback', function () { echo 'I need to be profiled!'; } );
 }
 
