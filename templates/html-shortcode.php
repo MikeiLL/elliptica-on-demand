e22842ad7249f1d2cfb781984d9fa929d6767eeb
Syed Muhammad Shafiq <shafiq_shaheen@hotmail.com>
2021-06-24 13:22:15 +0500

    template completed - basic

--- templates/html-shortcode.php
+++ templates/html-shortcode.php
@@ -1,77 +1,79 @@
+
+
 <div class="filters">
 
-	<div class="ui-group">
-
-		<div class="button-group" data-filter-group="level">
-		<?php $count = count($difficulty_levels); ?>
-		<button style="float:left;" class="button is-checked" data-filter="*">Any Level</button>
-		<?php $all_terms = [];
-		if ( $count > 0 ){
-			foreach ( $difficulty_levels as $term ) {
-				$termname = strtolower($term->name);
-				$termname = str_replace(' ', '-', $termname);
-				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
-				$all_terms[$count] = $termname;
-				$count--;
-			}
-		echo '<button style="float:left;" class="button" data-filter=".'.implode('.', $all_terms).'">All Levels Welcome</button>';
-		} ?>
-		</div> <!-- button-group -->
-
-		</div> <!-- ui-group -->
-		
-		
-		<div class="ui-group">
-
-		<div class="button-group" data-filter-group="instructor">
-		<?php $count = count($instructors);?>
-		<button style="float:left;" class="button is-checked" data-filter="*">Any Instructor</button>
-		<?php if ( $count > 0 ){
-			foreach ( $instructors as $term ) {
-				$termname = strtolower($term->name);
-				$termname = str_replace(' ', '-', $termname);
-				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
-
-			}
-		}?>
-		</div> <!-- button-group -->
-
-		</div> <!-- ui-group -->
-
-		<div class="ui-group">
-
-		<div class="button-group" data-filter-group="music">
-		<?php $count = count($music_styles);?>
-		<button style="float:left;" class="button is-checked" data-filter="*">All Styles</button>
-		<?php if ( $count > 0 ){
-			foreach ( $music_styles as $term ) {
-				$termname = strtolower($term->name);
-				$termname = str_replace(' ', '-', $termname);
-				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
-
-			}
-		}?>
-		</div> <!-- button-group -->
-
-		</div> <!-- ui-group -->		
-		
-		
-		<div class="ui-group">
-
-		<div class="button-group" data-filter-group="length">
-		<?php $count = count($class_lengths);?>
-		<button style="float:left;" class="button is-checked" data-filter="*">Any Length</button>
-		<?php if ( $count > 0 ){
-			foreach ( $class_lengths as $term ) {
-				$termname = strtolower($term->name);
-				$termname = str_replace(' ', '-', $termname);
-				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
-
-			}
-		}?>
-		</div> <!-- button-group -->
-
-		</div> <!-- ui-group -->
+    	<div class="ui-group">
+    
+    		<div class="button-group" data-filter-group="level">
+        		<?php $count = count($difficulty_levels); ?>
+        		<button style="float:left;" class="button is-checked" data-filter="*">Any Level</button>
+        		<?php $all_terms = [];
+        		if ( $count > 0 ){
+        			foreach ( $difficulty_levels as $term ) {
+        				$termname = strtolower($term->name);
+        				$termname = str_replace(' ', '-', $termname);
+        				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
+        				$all_terms[$count] = $termname;
+        				$count--;
+        			}
+        		echo '<button style="float:left;" class="button" data-filter=".'.implode('.', $all_terms).'">All Levels Welcome</button>';
+        		} ?>
+    		</div> <!-- button-group -->
+    
+    	</div> <!-- ui-group -->
+    		
+    		
+    	<div class="ui-group">
+    
+    		<div class="button-group" data-filter-group="instructor">
+    		<?php $count = count($instructors);?>
+    		<button style="float:left;" class="button is-checked" data-filter="*">Any Instructor</button>
+    		<?php if ( $count > 0 ){
+    			foreach ( $instructors as $term ) {
+    				$termname = strtolower($term->name);
+    				$termname = str_replace(' ', '-', $termname);
+    				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
+    
+    			}
+    		}?>
+    		</div> <!-- button-group -->
+    
+    	</div> <!-- ui-group -->
+    
+    	<div class="ui-group">
+    
+    		<div class="button-group" data-filter-group="music">
+    		<?php $count = count($music_styles);?>
+    		<button style="float:left;" class="button is-checked" data-filter="*">All Styles</button>
+    		<?php if ( $count > 0 ){
+    			foreach ( $music_styles as $term ) {
+    				$termname = strtolower($term->name);
+    				$termname = str_replace(' ', '-', $termname);
+    				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
+    
+    			}
+    		}?>
+    		</div> <!-- button-group -->
+    
+    	</div> <!-- ui-group -->		
+    		
+    		
+    	<div class="ui-group">
+    
+    		<div class="button-group" data-filter-group="length">
+    		<?php $count = count($class_lengths);?>
+    		<button style="float:left;" class="button is-checked" data-filter="*">Any Length</button>
+    		<?php if ( $count > 0 ){
+    			foreach ( $class_lengths as $term ) {
+    				$termname = strtolower($term->name);
+    				$termname = str_replace(' ', '-', $termname);
+    				echo '<button style="float:left;" class="button" data-filter=".'.$termname.'">' . $term->name . '</button>';
+    
+    			}
+    		}?>
+    		</div> <!-- button-group -->
+    
+    	</div> <!-- ui-group -->
 	</div> <!-- // filters -->		
 			
 			
@@ -102,48 +104,43 @@
 			}
 ?>
 	<a data-modaal-content-source="#modal-id-<?php echo $modaal_id_count;?>" href="#modal-id-<?php echo $modaal_id_count;?>" class="<?php echo $isotope_filter_classes;?> info-popup isotope_video_item">
-			<div class="od-video" data-modaal-type="inline" data-modaal-animation="fade" class="modaal"
 			<?php if (has_post_thumbnail( $post_id ) ):
 				$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'on_demand_video' );
-			  	echo 'style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)),url(' . $featured_image[0] .')"';
+			  	$custom_style = 'style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)),url(' . $featured_image[0] .')"';
 			else:
-				echo 'style="background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7))"';
-				endif; ?>>
+			$custom_style =  'style="background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7))"';
+				endif; ?>
 
-			<div class="od-video_info">
-<?php 
-			$class_title = ''; //get_the_title();
+			<div class="od-video" data-modaal-type="inline" data-modaal-animation="fade" class="modaal" <?php echo $custom_style;?>>
 
-			//$return .= '		<h5>' . $class_title . '</h5>';
-
-			$instructor_and_type = '';
-
-			$class_instructors = get_the_terms( $post_id, 'class_instructor' );
-			if ( !empty($class_instructors) ) {
-				foreach ( $class_instructors as $class_instructor ) {
-					$instructor_and_type .= $class_instructor->name . ' ';
-				}
-			}
-
-			$instructor_and_type .= '		<span aria-hidden="true"> · </span>';
-
-			$class_type = get_post_meta( $post_id, $prefix . MMC_TEXTDOMAIN . '_class_type' );
-			if ( !empty($class_type) ) {
-				$instructor_and_type .= $class_type[0] . ' ';
-			}
-
-			$date_time = get_post_meta( $post_id, $prefix . MMC_TEXTDOMAIN . '_date' );
-			if ( !empty($date_time) ) {
-				$instructor_and_type .= "<br/>";
-				$instructor_and_type .= date_i18n('F j', $date_time[0]) . ' @ ' . date_i18n('g:i a', $date_time[0]);
-			}
-
-			echo $instructor_and_type;
-?>
-			</div> <!-- //od-video_info -->
+    			<div class="od-video_info">
+        			
+        			<?php 
+        			$instructor_and_type = '';
+                    $class_instructors = get_the_terms( $post_id, 'class_instructor' );
+        			if ( !empty($class_instructors) ) {
+        				foreach ( $class_instructors as $class_instructor ) {
+        				    $instructor_and_type .= $class_instructor->name . ' ';
+        				}
+        			}
+                    ?>
+        			<span aria-hidden="true"> · </span>
+        			
+        			<?php 
+        			$class_type = get_post_meta( $post_id, $prefix . MMC_TEXTDOMAIN . '_class_type' );
+        			if ( !empty($class_type) ) {
+        				echo $class_type[0] . ' ';
+        			}
+        
+        			$date_time = get_post_meta( $post_id, $prefix . MMC_TEXTDOMAIN . '_date' );
+        			if ( !empty($date_time) ) {
+        				echo "<br/>";
+        				echo date_i18n('F j', $date_time[0]) . ' @ ' . date_i18n('g:i a', $date_time[0]);
+        			} ?>
+    			</div> <!-- //od-video_info -->
 
 			</div> <!-- //od-video -->
-			</a>
+	</a>
 <?php
 			$class_desc = get_post_meta( $post_id, $prefix . MMC_TEXTDOMAIN . '_desc' );
 			if ( !empty($class_desc) ) {
@@ -272,6 +269,4 @@
 
 		<?php endwhile; endif;?>
 	
-			
-			
 			
