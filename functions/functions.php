f23001e781436fa096a17f863116a0a2c25dbc4e
mikeill <mike@mzoo.org>
2021-08-28 18:27:51 -0500

    Php codesniffer fixes, mostly whitespace.

--- functions/functions.php
+++ functions/functions.php
@@ -33,58 +33,66 @@ function mmc_get_settings() {
  *
  * return array @minimized_class_plan
  */
-function minimize_and_sum_class_plans($class_plans = []) {
+function minimize_and_sum_class_plans( $class_plans = array() ) {
+	if ( empty( $class_plans ) ) {
+		return false;
+	}
 
-	if (empty($class_plans)) return false;
+	if ( is_array( $class_plans[0] ) && ! empty( $class_plans[0] ) ) {
+		$segments_only = array_map(
+			function ( $array ) {
+				$allowed      = array( 'segment_duration', 'segment_type' );
+				$segment_info = array_filter(
+					$array,
+					function ( $key ) use ( $allowed ) {
+						return in_array( $key, $allowed );
+					},
+					ARRAY_FILTER_USE_KEY
+				);
 
-	if (is_array($class_plans[0]) && !empty($class_plans[0])) {
+				array_walk(
+					$segment_info,
+					function( &$item, $key ) {
+						if ( $key === 'segment_duration' ) {
+							  $seconds = '00';
+							  $minutes = $item;
+							if ( strpos( $item, ':' ) ) {
+								$seconds = substr( $item, strpos( $item, ':' ) + 1 );
+								$minutes = substr( $item, 0, strpos( $item, ':' ) );
+							}
 
-		$segments_only = array_map( function ($array){
-			$allowed  = ['segment_duration', 'segment_type'];
-			$segment_info = array_filter($array,
-				function ($key) use ($allowed) {
-					return in_array($key, $allowed);
-				},
-				ARRAY_FILTER_USE_KEY
-			);
-
-			array_walk($segment_info, function(&$item, $key){
-				if ($key === 'segment_duration') {
-					$seconds = '00';
-					$minutes = $item;
-					if (strpos($item, ":")) {
-						$seconds = substr($item, strpos($item, ":") + 1);
-						$minutes = substr($item, 0, strpos($item, ":"));
+							$rounded = date( 'i', round( strtotime( '00:' . $minutes . ':' . $seconds ) / 60 ) * 60 );
+							$item    = intval( $rounded );
+						}
 					}
-					$rounded = date('i', round(strtotime('00:' . $minutes . ':' . $seconds)/60)*60);
-					$item = intval($rounded);
-				}
-			});
+				);
 
-			return $segment_info;
-
-		}, $class_plans[0] );
+				return $segment_info;
+			},
+			$class_plans[0]
+		);
 
 		// Now let's concatenate the subsequent occurences of matching keys
-		$minimized_class_plan = [];
-		foreach ($segments_only as $key => $val) {
-			if ( !empty($prev_seg_type) && $prev_seg_type == $val['segment_type']) {
+		$minimized_class_plan = array();
+		foreach ( $segments_only as $key => $val ) {
+			if ( ! empty( $prev_seg_type ) && $prev_seg_type == $val['segment_type'] ) {
 				// Remove the previous one, and add new one with summed duration.
-				array_pop($minimized_class_plan);
-				$minimized_class_plan[] = [
-					'segment_type' => $val['segment_type'],
-					'segment_duration' => $val['segment_duration'] + $prev_seg_duration
-				];
-				$prev_seg_type = $val['segment_type'];
-				$prev_seg_duration = $val['segment_duration'];
+				array_pop( $minimized_class_plan );
+				$minimized_class_plan[] = array(
+					'segment_type'     => $val['segment_type'],
+					'segment_duration' => $val['segment_duration'] + $prev_seg_duration,
+				);
+				$prev_seg_type          = $val['segment_type'];
+				$prev_seg_duration      = $val['segment_duration'];
 				continue;
 			}
-			$minimized_class_plan[] = [
-				'segment_type' => $val['segment_type'],
-				'segment_duration' => $val['segment_duration']
-			];
-			$prev_seg_type = $val['segment_type'];
-			$prev_seg_duration = $val['segment_duration'];
+
+			$minimized_class_plan[] = array(
+				'segment_type'     => $val['segment_type'],
+				'segment_duration' => $val['segment_duration'],
+			);
+			$prev_seg_type          = $val['segment_type'];
+			$prev_seg_duration      = $val['segment_duration'];
 		}
 
 		return $minimized_class_plan;
@@ -102,30 +110,32 @@ function minimize_and_sum_class_plans($class_plans = []) {
  *
  * @since 1.0.0
  *
- * @param 	string 	$template_name			Template to load.
- * @param 	string 	$string $template_path	Path to templates.
- * @param 	string	$default_path			Default path to template files.
- * @return 	string 							Path to the template file.
+ * @param   string $template_name          Template to load.
+ * @param   string $string $template_path  Path to templates.
+ * @param   string $default_path           Default path to template files.
+ * @return  string                          Path to the template file.
  */
 function eod_locate_template( $template_name, $template_path = '', $default_path = '' ) {
-    // Set variable to search in woocommerce-plugin-templates folder of theme.
-    if ( ! $template_path ) :
-    $template_path = 'elliptica/';
-    endif;
-    // Set default plugin templates path.
-    if ( ! $default_path ) :
-    $default_path = plugin_dir_path( dirname( __FILE__ ) ) . 'templates/'; // Path to the template folder
-    endif;
-    // Search template file in theme folder.
-    $template = locate_template( array(
-        $template_path . $template_name,
-        $template_name
-    ) );
-    // Get plugins template file.
-    if ( ! $template ) :
-    $template = $default_path . $template_name;
-    endif;
-    return apply_filters( 'eod_locate_template', $template, $template_name, $template_path, $default_path );
+	// Set variable to search in woocommerce-plugin-templates folder of theme.
+	if ( ! $template_path ) :
+		$template_path = 'elliptica/';
+	endif;
+	// Set default plugin templates path.
+	if ( ! $default_path ) :
+		$default_path = plugin_dir_path( dirname( __FILE__ ) ) . 'templates/'; // Path to the template folder
+	endif;
+	// Search template file in theme folder.
+	$template = locate_template(
+		array(
+			$template_path . $template_name,
+			$template_name,
+		)
+	);
+	// Get plugins template file.
+	if ( ! $template ) :
+		$template = $default_path . $template_name;
+	endif;
+	return apply_filters( 'eod_locate_template', $template, $template_name, $template_path, $default_path );
 }
 /**
  * Get template.
@@ -136,21 +146,21 @@ function eod_locate_template( $template_name, $template_path = '', $default_path
  *
  * @see eod_locate_template()
  *
- * @param string 	$template_name			Template to load.
- * @param array 	$args					Args passed for the template file.
- * @param string 	$string $template_path	Path to templates.
- * @param string	$default_path			Default path to template files.
+ * @param string $template_name          Template to load.
+ * @param array  $args                   Args passed for the template file.
+ * @param string $string $template_path  Path to templates.
+ * @param string $default_path           Default path to template files.
  */
 function eod_get_template( $template_name, $args = array(), $tempate_path = '', $default_path = '' ) {
-    if ( is_array( $args ) && isset( $args ) ) :
-    extract( $args );
-    endif;
-    $template_file = eod_locate_template( $template_name, $tempate_path, $default_path );
-    if ( ! file_exists( $template_file ) ) :
-    _doing_it_wrong( __FUNCTION__, sprintf( '<code>%s</code> does not exist.', $template_file ), '1.0.0' );
-    return;
-    endif;
-    include $template_file;
+	if ( is_array( $args ) && isset( $args ) ) :
+		extract( $args );
+	endif;
+	$template_file = eod_locate_template( $template_name, $tempate_path, $default_path );
+	if ( ! file_exists( $template_file ) ) :
+		_doing_it_wrong( __FUNCTION__, sprintf( '<code>%s</code> does not exist.', $template_file ), '1.0.0' );
+		return;
+	endif;
+	include $template_file;
 }
 
 
