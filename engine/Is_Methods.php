bcf7b3adba9c0326eb545c93fa7ae4998df46945
mikeill <mike@mzoo.org>
2021-09-02 18:27:21 -0500

    Escape namespace, based on current parent fork codebase.

--- engine/Is_Methods.php
+++ engine/Is_Methods.php
@@ -99,19 +99,20 @@ class Is_Methods {
 	 * @return boolean
 	 */
 	public function is_rest() {
-		return true;
-		if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+		if ( \defined( 'REST_REQUEST' ) && REST_REQUEST ) {
 			return true;
 		}
 
 		global $wp_rewrite;
+
 		if ( $wp_rewrite === null ) {
-			$wp_rewrite = new \WP_Rewrite();
+			$wp_rewrite = new \WP_Rewrite; //phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		}
 
-		$rest_url    = wp_parse_url( trailingslashit( rest_url() ) );
-		$current_url = wp_parse_url( add_query_arg( array() ) );
-		return strpos( $current_url['path'], substr( $rest_url['path'], 0, strlen( $rest_url['path'] ) - 1 ) ) === 0;
+		$rest_url    = \wp_parse_url( \trailingslashit( \rest_url() ) );
+		$current_url = \wp_parse_url( \add_query_arg( array() ) );
+
+		return \strpos( $current_url[ 'path' ], \substr( $rest_url[ 'path' ], 0, \strlen( $rest_url[ 'path' ] ) - 1 ) ) === 0;
 	}
 
 	/**
