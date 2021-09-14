56d56b559a2af28ab2f07f5099460897b75854d3
mikeill <mike@mzoo.org>
2021-09-14 14:44:44 -0500

    Add start of HTML template for od_video grid element.

---
+++ templates/od-video-template.php
@@ -0,0 +1,105 @@
+<a data-modaal-content-source="#modal-id-1" href="#modal-id-1" class="">
+  <div class="od-video">
+    <div class="od-video_info">
+      <span aria-hidden="true"> ·
+      </span>
+    </div>
+    <!-- //od-video_info -->
+  </div>
+  <!-- //od-video -->
+</a>
+
+<!-- Modal Window -->
+<!-- /* Build the Modal Content */ -->
+<div id="modal-id-<?php esc_attr_e( $modaal_id_count ); ?>"
+		style="display: none;">
+		<?php
+		/* Header */
+
+		$difficulty_levels       = array();
+		$difficulty_levels_array = get_the_terms( $post_id, 'difficulty_level' );
+		if ( ! empty( $difficulty_levels_array ) ) {
+			foreach ( $difficulty_levels_array as $k => $difficulty_level ) {
+				$difficulty_levels[ $k ] = $difficulty_level->name . ' ';
+			}
+		}
+
+		$header_attr = 'style="background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7));"';
+		if ( has_post_thumbnail( $post_id ) ) :
+			$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'medium_large' );
+			if ( is_array( $featured_image ) ) {
+				$header_attr = 'style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)),url(' . $featured_image[0] . ')"';
+			}
+		endif;
+
+		?>
+		<!-- Build modal class header background declaration -->
+		<div class="modal-class-details__header" <?php echo $header_attr; ?>>
+
+
+
+			<div class="modal-class-details__header_content">
+				<div>
+					<!-- <h5 style="class-title"></h5> -->
+					<p class="instructor_and_type"></p>
+				</div>
+				<div>
+					<a class="modal_header_play_button"
+						href="https://video.mindbody.io/studios/526618/videos/<?php echo $video_link; ?>">Start</a>
+					<!-- <svg viewBox="0 0 28 28" width="16" height="16"><g stroke="none" stroke-width="1" fill-rule="evenodd"><path d="M23.3870324,12.1022967 C25.1944355,13.1466985 25.1944355,14.8547015 23.3870324,15.8977033 L8.04720582,24.7541186 C6.23840269,25.7985204 4.76000013,24.9445189 4.76000013,22.8571153 L4.76000013,5.14288467 C4.76000013,3.05548105 6.23840269,2.20147957 8.04720582,3.24588138 L23.3870324,12.1022967 Z" fill="#ffffff"></path></g></svg> -->
+				</div>
+			</div>
+		</div>
+
+		<div class="modal-class-details__body">
+
+			<div class="modal-class-details__intro">
+				<div class="modal-class-details__intro__level">
+					<p>
+						<strong>Level</strong>:</p>
+				</div>
+				<div class="modal-class-details__intro__desc">
+					<p></p>
+
+				</div>
+			</div>
+
+		<?php
+		/* // Intro */
+
+		/* Playlist */
+			?>
+			<div class="modal-class-details__playlist_section">
+				<h2>Playlist</h2>
+				<div class="modal-class-details__listwrap mcd__playlist_hideContent">
+					<ol>
+							<li class="modal-class-details__playlist_item"><img
+							class="modal-class-details__playlist_img"
+							src="" />
+							<div class="modal-class-details__song_details">
+								<strong></strong> <span></span>
+							</div></li>
+
+						</ol>
+				</div>
+
+					<a href="#">Show more</a>
+				</div>
+					</div>
+
+			<div class="modal-class-details__classplan_section">
+				<h2>Class Plan</h2>
+				<div class="modal-class-details__listwrap">
+					<ol>
+					<li class="modal-class-details__classplan_item">
+							<div class="modal-class-details__segment_type">
+						</div>
+							<div class="modal-class-details__segment_duration">
+						</div>
+						</li>
+				</ol>
+				</div>
+			</div>
+
+		</div>
+	</div>
